f955cd0ecc3cb040336882cd6dcd13612184a045
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-19 19:38:23 +0100

    format

--- include/ft_algorithms.h
+++ include/ft_algorithms.h
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 18:31:31 by poss              #+#    #+#             */
-/*   Updated: 2023/12/17 13:50:09 by poss             ###   ########.fr       */
+/*   Updated: 2023/12/19 19:35:18 by poss             ###   ########.fr       */
 /*   Updated: 2023/12/15 20:48:19 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -17,7 +17,11 @@
 # include <stdbool.h>
 # include <stddef.h>
 
-bool	is_sorted(void *array, size_t size, size_t element_size,
-			int (*compare)(void *, void *));
+typedef bool	(*t_unary_predicate)(void *);
+typedef bool	(*t_binary_predicate)(void *, void *);
+typedef int		(*t_comparison)(void *, void *);
+
+bool			is_sorted(void *array, size_t size, size_t element_size,
+					t_comparison);
 
 #endif
--- test/src/sort.cpp
+++ test/src/sort.cpp
@@ -2,28 +2,33 @@
 
 #include <vector>
 
-extern "C" {
+extern "C"
+{
 #include "ft_algorithms.h"
 };
 
-int int_compare_C_style(void* int1, void* int2) {
-	int __int1 = *(int*)int1;
-	int __int2 = *(int*)int2;
+int int_compare_C_style(void* int1, void* int2)
+{
+    int __int1 = *(int*)int1;
+    int __int2 = *(int*)int2;
 
-	if (__int1 < __int2)
-		return -1;
-	if (__int1 > __int2)
-		return 1;
-	return 0;
+    if (__int1 < __int2)
+        return -1;
+    if (__int1 > __int2)
+        return 1;
+    return 0;
 }
 
-TEST(Sorts, IsSorted) {
-	int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-	int array_size = sizeof(array) / sizeof(int);
-	std::vector<int> array__ {array, array + array_size};
+TEST(Sorts, IsSorted)
+{
+    int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int array_size = sizeof(array) / sizeof(int);
+    std::vector<int> array__{array, array + array_size};
 
-	bool expected = std::is_sorted(array__.cbegin(), array__.cend());
-	bool actual = is_sorted(array, array_size, sizeof(*array), &int_compare_C_style);
+    bool expected = std::is_sorted(array__.cbegin(), array__.cend());
+    bool actual =
+        is_sorted(array, array_size, sizeof(*array), &int_compare_C_style);
 
-	ASSERT_EQ(expected, actual) << "Error, expected " << expected << " was " << actual << '\n';
+    ASSERT_EQ(expected, actual)
+        << "Error, expected " << expected << " was " << actual << '\n';
 }
