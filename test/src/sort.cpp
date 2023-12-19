ce6914cae19aef10491912be609c0d1dbc326b77
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-19 20:55:35 +0100

    more tests

--- test/src/sort.cpp
+++ test/src/sort.cpp
@@ -6,13 +6,35 @@ extern "C"
 #include "ft_predicates.h"
 };
 
-TEST(Sorts, IsSorted)
+TEST(Sorts, IsSorted_NotSorted)
 {
-    int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int array_size = sizeof(array) / sizeof(int);
+    int array1[] = {1, 4, 5, 7, 23, 4, 6, 3, 4, 6, 3};
+    int array_size = sizeof(array1) / sizeof(int);
 
     ASSERT_EQ(false,
-              is_sorted(array, array_size, &less_equal_int, sizeof(*array)));
+              is_sorted(array1, array_size, &less_equal_int, sizeof(*array1)));
+    ASSERT_EQ(false, is_sorted(array1, array_size, &greater_equal_int,
+                               sizeof(*array1)));
+}
+
+TEST(Sorts, IsSorted_Sorted)
+{
+    int array1[] = {1, 2, 3, 4, 5};
+    int array_size = sizeof(array1) / sizeof(int);
+
     ASSERT_EQ(true,
-              is_sorted(array, array_size, &greater_equal_int, sizeof(*array)));
+              is_sorted(array1, array_size, &less_equal_int, sizeof(*array1)));
+    ASSERT_EQ(false, is_sorted(array1, array_size, &greater_equal_int,
+                               sizeof(*array1)));
+}
+
+TEST(Sorts, IsSorted_SortedBackward)
+{
+    int array1[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int array_size = sizeof(array1) / sizeof(int);
+
+    ASSERT_EQ(false,
+              is_sorted(array1, array_size, &less_equal_int, sizeof(*array1)));
+    ASSERT_EQ(true, is_sorted(array1, array_size, &greater_equal_int,
+                              sizeof(*array1)));
 }
