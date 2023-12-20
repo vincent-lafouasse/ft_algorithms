e4651c0be2b1cfb75473330c501c712657c89f93
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-20 17:27:25 +0100

    test

--- test/src/sort.cpp
+++ test/src/sort.cpp
@@ -11,8 +11,10 @@ TEST(IsSorted, NotSorted)
     int array[] = {1, 4, 5, 7, 23, 4, 6, 3, 4, 6, 3};
     size_t array_size = sizeof(array) / sizeof(int);
 
-    ASSERT_EQ(false, is_sorted({array, array_size, sizeof(int)}, &less_equal_int));
-    ASSERT_EQ(false, is_sorted({array, array_size, sizeof(int)}, &greater_equal_int));
+    ASSERT_EQ(false,
+              is_sorted({array, array_size, sizeof(int)}, &less_equal_int));
+    ASSERT_EQ(false,
+              is_sorted({array, array_size, sizeof(int)}, &greater_equal_int));
 }
 
 TEST(IsSorted, Sorted)
@@ -20,8 +22,10 @@ TEST(IsSorted, Sorted)
     int array[] = {1, 2, 3, 4, 5};
     size_t array_size = sizeof(array) / sizeof(int);
 
-    ASSERT_EQ(true, is_sorted({array, array_size, sizeof(int)}, &less_equal_int));
-    ASSERT_EQ(false, is_sorted({array, array_size, sizeof(int)}, &greater_equal_int));
+    ASSERT_EQ(true,
+              is_sorted({array, array_size, sizeof(int)}, &less_equal_int));
+    ASSERT_EQ(false,
+              is_sorted({array, array_size, sizeof(int)}, &greater_equal_int));
 }
 
 TEST(IsSorted, SortedBackward)
@@ -29,6 +33,8 @@ TEST(IsSorted, SortedBackward)
     int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
     size_t array_size = sizeof(array) / sizeof(int);
 
-    ASSERT_EQ(false, is_sorted({array, array_size, sizeof(int)}, &less_equal_int));
-    ASSERT_EQ(true, is_sorted({array, array_size, sizeof(int)}, &greater_equal_int));
+    ASSERT_EQ(false,
+              is_sorted({array, array_size, sizeof(int)}, &less_equal_int));
+    ASSERT_EQ(true,
+              is_sorted({array, array_size, sizeof(int)}, &greater_equal_int));
 }
