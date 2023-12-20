001ac94a618082d2a10d8ec46866c6afb01a62e6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-20 23:53:54 +0100

    minor refactor

--- include/ft_algorithms.h
+++ include/ft_algorithms.h
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 18:31:31 by poss              #+#    #+#             */
-/*   Updated: 2023/12/20 17:49:20 by poss             ###   ########.fr       */
+/*   Updated: 2023/12/20 23:53:44 by poss             ###   ########.fr       */
 /*   Updated: 2023/12/15 20:48:19 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -24,13 +24,13 @@ typedef struct
 	size_t	element_size;
 }			t_array;
 
-bool		is_sorted(const t_array array, t_binary_predicate less_equal);
-
 bool		all(const t_array array, t_unary_predicate pred);
 bool		any(const t_array array, t_unary_predicate pred);
 bool		none(const t_array array, t_unary_predicate pred);
 
 void		map(t_array array, void (*transform)(void *));
 void		map_copy(t_array src, void *dest, void (*transform)(void *));
+
+bool		is_sorted(const t_array array, t_binary_predicate less_equal);
 
 #endif
